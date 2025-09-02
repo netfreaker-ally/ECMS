66144a093dd901a407f13348dcd5e912849cb731
Hanuma Ramavath <hanumaramavath@Hanumas-MacBook-Air.local>
2025-09-03 00:56:50 +0530

    Add redis instance config

--- ecms/src/main/java/com/sidecar/ecms/EcmsApplication.java
+++ ecms/src/main/java/com/sidecar/ecms/EcmsApplication.java
@@ -2,8 +2,11 @@ package com.sidecar.ecms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableCaching
 public class EcmsApplication {
 
 	public static void main(String[] args) {
